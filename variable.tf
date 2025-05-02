b4ba4248675aeca330107a6605ddd04fd931b7d1
12Niki <133602046+12Niki@users.noreply.github.com>
2025-05-03 00:06:57 +0530

    Update variable.tf

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+
+variable "gcp_project" {}
+variable "gcp_region" {
+  default = "us-central1"
+}
